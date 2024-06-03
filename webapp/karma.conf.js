a209549b932f72d7bc00f743a3365854fcf8e687
ala jammali <ala.jammali@aymax.fr>
2024-06-03 13:29:52 +0000

    test modif code

--- webapp/karma.conf.js
+++ webapp/karma.conf.js
@@ -1,6 +1,11 @@
 module.exports = function(config) {
   config.set({
     frameworks: ["qunit", "ui5", "sinon"],
+    plugins: [
+      require('karma-qunit'),
+      require('karma-ui5'),
+      require('karma-sinon')
+    ],
     ui5: {
       url: "https://openui5.hana.ondemand.com",
       mode: "html",
