6fe7cf348cab0bf9ec13d43df8df6b9161e2df58
ala jammali <ala.jammali@aymax.fr>
2024-06-03 14:17:00 +0000

    update karma.conf

--- webapp/karma.conf.js
+++ webapp/karma.conf.js
@@ -1,24 +1,10 @@
 module.exports = function(config) {
   config.set({
-    frameworks: ["qunit", "ui5", "sinon"],
-    plugins: [
-      require('karma-qunit'),
-      require('karma-ui5'),
-      require('karma-sinon')
-    ],
-    ui5: {
-      url: "https://openui5.hana.ondemand.com",
-      mode: "html",
-      config: {
-        async: true,
-        resourceRoots: {
-          "sap.ui.demo.todo": "./base/webapp"
-        }
-      },
-      tests: [
-        "sap/ui/demo/todo/test/unit/AllTests"
-      ]
-    },
+
+    frameworks: ["ui5"],
+
+
     browsers: ["Chrome"]
+
   });
 };
