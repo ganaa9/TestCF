c2e2dec914014ff333e0dd426eda7c0fbe5cbc31
ala jammali <ala.jammali@aymax.fr>
2024-06-03 16:07:40 +0000

    updating karma-conf.js

--- webapp/karma-ci.conf.js
+++ webapp/karma-ci.conf.js
@@ -1,11 +1,35 @@
 module.exports = function(config) {
+    require("./karma.conf")(config);
+    
     config.set({
-  
-     //..
-  
-  
-      browsers: ["ChromeHeadless"],
-      singleRun: true
-  
+        preprocessors: {
+            "{webapp,webapp/!(test)}/*.js": ["coverage"]
+        },
+        
+        coverageReporter: {
+            includeAllSources: true,
+            reporters: [
+                { 
+                    type: "html",
+                    dir: "coverage"
+                },
+                {
+                    type: "text"
+                }
+            ],
+            check: {
+                each: {
+                    statements: 100,
+                    branches: 100,
+                    functions: 100,
+                    lines: 100
+                }
+            }
+        },
+
+        reporters: ["progress","coverage"],
+
+        browsers: ["ChromeHeadless"],
+        singleRun: true
     });
-  };
+};
